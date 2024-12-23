17e7e1e8cf0e0acc16ce2a5f8cb97bd3dff27ecc
Benjamin McLemore <51517288+Cheimonophile@users.noreply.github.com>
2024-12-23 16:22:58 -0500

    collect water

--- core/src/main/java/com/benjaminmclemore/fourx/Main.java
+++ core/src/main/java/com/benjaminmclemore/fourx/Main.java
@@ -9,7 +9,10 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 
@@ -31,6 +34,10 @@ public class Main extends ApplicationAdapter {
   Music music;
   Sprite bucketSprite; // Declare a new Sprite variable
   Vector2 touchPos;
+  Array<Sprite> dropSprites;
+  float dropTimer;
+  Rectangle bucketRectangle;
+  Rectangle dropRectangle;
 
   @Override
   public void create() {
@@ -47,6 +54,10 @@ public class Main extends ApplicationAdapter {
     bucketSprite = new Sprite(bucketTexture); // Initialize the sprite based on the texture
     bucketSprite.setSize(1, 1); // Define the size of the sprite
     touchPos = new Vector2();
+    dropSprites = new Array<>();
+    bucketRectangle = new Rectangle();
+    dropRectangle = new Rectangle();
+
   }
 
   @Override
@@ -88,6 +99,48 @@ public class Main extends ApplicationAdapter {
 
   private void logic() {
 
+    // Store the worldWidth and worldHeight as local variables for brevity
+    float worldWidth = viewport.getWorldWidth();
+    float worldHeight = viewport.getWorldHeight();
+
+    // Store the bucket size for brevity
+    float bucketWidth = bucketSprite.getWidth();
+    float bucketHeight = bucketSprite.getHeight();
+
+    // Subtract the bucket width
+    bucketSprite.setX(MathUtils.clamp(bucketSprite.getX(), 0, worldWidth - bucketWidth));
+
+    // retrieve the current delta
+    float delta = Gdx.graphics.getDeltaTime();
+
+    // Apply the bucket position and size to the bucketRectangle
+    bucketRectangle.set(bucketSprite.getX(), bucketSprite.getY(), bucketWidth, bucketHeight);
+
+    // Loop through the sprites backwards to prevent out of bounds errors
+    for (int i = dropSprites.size - 1; i >= 0; i--) {
+      Sprite dropSprite = dropSprites.get(i); // Get the sprite from the list
+      float dropWidth = dropSprite.getWidth();
+      float dropHeight = dropSprite.getHeight();
+
+      dropSprite.translateY(-2f * delta);
+
+      // Apply the drop position and size to the dropRectangle
+      dropRectangle.set(dropSprite.getX(), dropSprite.getY(), dropWidth, dropHeight);
+
+      // if the top of the drop goes below the bottom of the view, remove it
+      if (dropSprite.getY() < -dropHeight)
+        dropSprites.removeIndex(i);
+      else if (bucketRectangle.overlaps(dropRectangle)) // Check if the bucket overlaps the drop
+        dropSprites.removeIndex(i); // Remove the drop
+
+    }
+
+    dropTimer += delta; // Adds the current delta to the timer
+    if (dropTimer > 1f) { // Check if it has been more than a second
+      dropTimer = 0; // Reset the timer
+      createDroplet(); // Create the droplet
+    }
+
   }
 
   private void draw() {
@@ -104,7 +157,27 @@ public class Main extends ApplicationAdapter {
     spriteBatch.draw(backgroundTexture, 0, 0, worldWidth, worldHeight);
     bucketSprite.draw(spriteBatch); // Sprites have their own draw method
 
+    // draw each sprite
+    for (Sprite dropSprite : dropSprites) {
+      dropSprite.draw(spriteBatch);
+    }
+
     spriteBatch.end();
 
+  }
+
+  private void createDroplet() {
+    // create local variables for convenience
+    float dropWidth = 1;
+    float dropHeight = 1;
+    float worldWidth = viewport.getWorldWidth();
+    float worldHeight = viewport.getWorldHeight();
+
+    // create the drop sprite
+    Sprite dropSprite = new Sprite(dropTexture);
+    dropSprite.setSize(dropWidth, dropHeight);
+    dropSprite.setX(MathUtils.random(0f, worldWidth - dropWidth)); // Randomize the drop's x position
+    dropSprite.setY(worldHeight);
+    dropSprites.add(dropSprite); // Add it to the list
   }
 }
